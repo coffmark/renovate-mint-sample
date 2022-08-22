4aa06ecdf96961d5c5851f87f0b5cec0b1afedaa
coffmark <come1234560@gmail.com>
2022-08-23 07:24:21 +0900

    initial commit

---
+++ RenovateMintSample/RenovateMintSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenovateMintSampleApp.swift
+//  RenovateMintSample
+//
+//  Created by 神村　亮佑　 on 2022/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RenovateMintSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
